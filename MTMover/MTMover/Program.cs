622a945af59995c21234757444979fd344b4b624
Costa <costa37@gmail.com>
2015-08-01 07:10:53 +0300

    Folder location for test was added
    Began with the first step

--- MTMover/MTMover/Program.cs
+++ MTMover/MTMover/Program.cs
@@ -14,9 +14,22 @@ namespace MTMover
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            Application.EnableVisualStyles(); //Activates some visual affects that can be used for buttons as example: http://stackoverflow.com/questions/6764100/does-application-enablevisualstyles-do-anything
+            Application.SetCompatibleTextRenderingDefault(false); //Rendering the text visualy better and support for localisation:http://stackoverflow.com/questions/2341728/application-setcompatibletextrenderingdefaultfalse  
             Application.Run(new Form1());
+            
+            //Begin of my code
+
+            //http://www.dotnetperls.com/path
+            string locationfilepath = @"H:\Chrom Downloads\C#\For testing\locationfile";
+
+
+
+
+
+
+
+            
         }
     }
 }
